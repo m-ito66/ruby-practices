4ec7f0265183e6bb266b951ae1938a8e34149f40
m-ito <m-ito@ruby-dev.jp>
2021-07-25 23:55:59 +0900

    revise method names

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -10,71 +10,50 @@ def main
     opt.parse!(ARGV)
   end
   if ARGV.empty?
-    total_text_info = input_to_terminal
-    write_file_info(total_text_info, l_flag)
+    file_info = build_file_info(change_array_to_text(readlines), nil)
+    print_file_info(file_info, l_flag)
   else
     file_names = ARGV
-    file_info_list, total_info_list = read_file_info(file_names)
+    file_info_list, total_file_info = build_file_info_list(file_names)
     file_info_list.each do |file|
-      write_file_info(file, l_flag)
+      print_file_info(file, l_flag)
     end
-    write_file_info(total_info_list, l_flag) if ARGV.size >= 2
+    print_file_info(total_file_info, l_flag) if ARGV.size >= 2
   end
 end
 
-def input_to_terminal
-  stdin_text = readlines
-  line_count, word_count, bytesize = read_each_file(stdin_text)
+def build_file_info_list(file_names)
+  all_texts = []
+  file_info_list = file_names.map do |file_name|
+    file_text = change_array_to_text(File.read(file_name).lines)
+    all_texts << file_text
+    build_file_info(file_text, file_name)
+  end
+  total_text = change_array_to_text(all_texts, "\n")
+  total_file_info = build_file_info(total_text, 'total')
+  [file_info_list, total_file_info]
+end
+
+def build_file_info(text, name)
   {
-    line_count: line_count,
-    word_count: word_count,
-    bytesize: bytesize,
-    name: nil
+    line_count: text.lines.count,
+    word_count: text.split.size,
+    bytesize: text.bytesize,
+    name: name
   }
 end
 
-def read_file_info(file_names)
-  total_line_count = total_word_count = total_bytesize = 0
-  each_file_info = file_names.map do |file_name|
-    file_text = File.read(file_name).lines
-    line_count, word_count, bytesize = read_each_file(file_text)
-    total_line_count += line_count
-    total_word_count += word_count
-    total_bytesize += bytesize
-    {
-      line_count: line_count,
-      word_count: word_count,
-      bytesize: bytesize,
-      name: file_name
-    }
-  end
-  total_file_info =
-    {
-      line_count: total_line_count,
-      word_count: total_word_count,
-      bytesize: total_bytesize,
-      name: 'total'
-    }
-  [each_file_info, total_file_info]
-end
-
-def read_each_file(file)
-  line_count = word_count = bytesize = 0
-  file.each do |sentence|
-    line_count += 1
-    word_count += sentence.split.size
-    bytesize += sentence.bytesize
-  end
-  [line_count, word_count, bytesize]
-end
-
-def write_file_info(file, l_flag)
-  print format(file[:line_count])
+def print_file_info(file_info, l_flag)
+  print format(file_info[:line_count])
   unless l_flag
-    print format(file[:word_count])
-    print format(file[:bytesize])
+    print format(file_info[:word_count])
+    print format(file_info[:bytesize])
   end
-  puts " #{file[:name]}"
+  puts " #{file_info[:name]}"
+end
+
+def change_array_to_text(ary, str = nil)
+  ary.join(str)
 end
 
 def format(value)
