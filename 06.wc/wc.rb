840427115740bfb467c742af72f235f60f730716
m-ito <m-ito@ruby-dev.jp>
2021-07-25 16:55:59 +0900

    revise array to hash process

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -10,43 +10,51 @@ def main
     opt.parse!(ARGV)
   end
   if ARGV.empty?
-    total_text_info = change_to_hash(input_to_terminal)
+    total_text_info = input_to_terminal
     write_file_info(total_text_info, l_flag)
   else
     file_names = ARGV
     file_info_list, total_info_list = read_file_info(file_names)
     file_info_list.each do |file|
-      file = change_to_hash(file)
       write_file_info(file, l_flag)
     end
-    total_info_list = change_to_hash(total_info_list)
     write_file_info(total_info_list, l_flag) if ARGV.size >= 2
   end
-end
-
-def change_to_hash(array)
-  key = %i[line_count word_count bytesize name]
-  ary = [key, array].transpose
-  Hash[*ary.flatten]
 end
 
 def input_to_terminal
   stdin_text = readlines
   line_count, word_count, bytesize = read_each_file(stdin_text)
-  [line_count, word_count, bytesize, nil]
+  {
+    line_count: line_count,
+    word_count: word_count,
+    bytesize: bytesize,
+    name: nil
+  }
 end
 
 def read_file_info(file_names)
-  total_line_count = total_word_count = total_file_size = 0
+  total_line_count = total_word_count = total_bytesize = 0
   each_file_info = file_names.map do |file_name|
     file_text = File.read(file_name).lines
     line_count, word_count, bytesize = read_each_file(file_text)
     total_line_count += line_count
     total_word_count += word_count
-    total_file_size += bytesize
-    [line_count, word_count, bytesize, file_name]
+    total_bytesize += bytesize
+    {
+      line_count: line_count,
+      word_count: word_count,
+      bytesize: bytesize,
+      name: file_name
+    }
   end
-  total_file_info = [total_line_count, total_word_count, total_file_size, 'total']
+  total_file_info =
+    {
+      line_count: total_line_count,
+      word_count: total_word_count,
+      bytesize: total_bytesize,
+      name: 'total'
+    }
   [each_file_info, total_file_info]
 end
 
